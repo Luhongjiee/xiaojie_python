bb39540af9bade42fa332553d1ea1b3ed223b6df
Luhongjiee <herris0629@gmail.com>
2025-05-27 15:19:51 +0800

    Add files via upload

---
+++ 23,5,27,2.py
@@ -0,0 +1,12 @@
+stu1=["王大明",174,65]
+stu2=["林小華",168,58]
+stu3=["許中和",159,45]
+stu4=["趙小慧",159,45]
+
+body_info=[stu1,stu2,stu3,stu4]
+for info in body_info:
+    for item in info:
+        print(item," ",end="")
+    print()
+        
+    
